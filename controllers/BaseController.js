da2ffb55689a646df3e70990bb81bc6bbd99a283
nguyễn thái nhân <ntnhan90@gmail.com>
2023-09-10 14:46:00 +0700

    update Basecontroller
    xong các phương thức CURD, các controller khác kế thừa lại Base

--- controllers/BaseController.js
+++ controllers/BaseController.js
@@ -11,27 +11,107 @@ class BaseController{
 	}
 
 	static async getById(req, modelName) {
-
+		const reqParam = req.params.id;
+		let result;
+		try{
+			result = await req.app.get('db')[modelName].findByPk(reqParam).then(
+				errHandler.throwIf(r => !r, 404, 'not found', 'Resource not found'),
+				errHandler.throwError(500, 'sequelize error, something wrong with either the data base connection or schema')
+			)
+		}catch(err){
+			return Promise.reject(err);
+		}
+		return result
 	}
 
 	static async getByCustomOptions(req, modelName, options){
-
+		let result;
+		try{
+			result = await req.app.get('db')[modelName].findOne(options);
+		}catch(err){
+			return Promise.reject(err);
+		}
+		return result;
 	}
 
 	static async deleteById(req, modelName){
-
+		const reqParam = req.params.id;
+		let result;
+		try {
+			result = await req.app.get('db')[modelName].destroy({
+				where:{
+					id: reqParam
+				},
+			}).then(
+				errHandler.throwIf(r => r < 1, 404, 'not found', 'No record matches the Id provided'),
+				errHandler.throwError(500, 'sequelize error'),
+			)
+		}catch(err){
+			return Promise.reject(err);
+		}
+		return result;
 	}
 
 	static async create(req, modelName, data){
-
+		let obj = data;
+		if(_.isUndefined(obj)){
+			obj = req.body;
+		}
+		let result;
+		try{
+			result = await req.app.get('db')[modelName].build(obj).save().then(
+				errHandler.throwIf(r => !r, 500, 'Internal server error', 'something went wrong couldnt save data'),
+				errHandler.throwError(500, 'sequelize error'),
+			).then(
+				savedResource =>Promise.resolve(savedResource),
+			);
+		}catch(err){
+			return Promise.reject(err);
+		}
+		return result;
 	}
 
 	static async updateById(req, modelName, data){
+		const recordID = req.params.id;
+		let result;
 
+		try {
+			result = await req.app.get('db')[modelName]
+				.update(data, {
+					where: {
+						id: recordID,
+					},
+				}).then(
+					errHandler.throwIf(r => !r, 500, 'Internal server error', 'something went wrong couldnt update data'),
+					errHandler.throwError(500, 'sequelize error'),
+
+				).then(
+					updatedRecored => Promise.resolve(updatedRecored),
+				);
+		} catch (err) {
+			return Promise.reject(err);
+		}
+		return result;
 	}
 
 	static async updateByCustomWhere(req, modelName, data, options){
+		let result;
 
+		try {
+			result = await req.app.get('db')[modelName]
+				.update(data, {
+					where: options,
+				}).then(
+					errHandler.throwIf(r => !r, 500, 'Internal server error', 'something went wrong couldnt update data'),
+					errHandler.throwError(500, 'sequelize error'),
+
+				).then(
+					updatedRecored => Promise.resolve(updatedRecored),
+				);
+		} catch (err) {
+			return Promise.reject(err);
+		}
+		return result;
 	}
 
 	static async getList(req, modelName, options){
@@ -59,7 +139,7 @@ class BaseController{
 			results = await req.app.get('db')[modelName]
 				.findAll(options)
 				.then(
-					errHandler.throwError(r => !r, 500 , 'Internal serve error' , 'something went wrong while fetching data'),
+					errHandler.throwIf(r => !r, 500 , 'Internal serve error' , 'something went wrong while fetching data'),
 					errHandler.throwError(500, 'sequelize error')					
 				).then(result => Promise.resolve(result));
 		} catch(err){
