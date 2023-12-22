aeb5a1656577affb670a8fb5a9c958ba1053b284
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2023-12-22 16:14:25 +0700

    Repeat conflit fixing 4 (#10)
    * 4th commit - repeat-conflit-fixing-4
    
    ---------
    
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,7 @@
 module.exports = {
-    accessTokenSecret: "access-token-secret-example",
-    accessTokenLife: "10m",
-    refreshTokenSize: 100,
-}
+  accessTokenSecret: "access-token-secret-example",
+  accessTokenLife: "10m",
+  refreshTokenSize: 100,
+};
+
+// repeat-conflit-fixing-4
